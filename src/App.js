5094ee2335361a17013b5c4e65198dbe6969460d
hungbaonguyendn <hungbaonguyen.dn@gmail.com>
2020-08-16 23:32:55 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+
+function App() {
+  return (
+    <div className="grid-container">
+      <header>
+        <div className="brand">
+          <a href="#">React-shopping-cart</a>
+        </div>
+        <div className="header-links">
+           <a href="#">Signin</a>
+        </div>
+      </header>
+      <main>
+
+      </main>
+      <footer>
+        Allright Reserved.
+      </footer>
+     
+    </div>
+  );
+}
+
+export default App;
