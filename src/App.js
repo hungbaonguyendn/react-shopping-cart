3bdabfb607d04e6d2b7f87d077c47ef0eb281fa6
hungbaonguyendn <hungbaonguyen.dn@gmail.com>
2020-08-18 23:05:33 +0700

    add redux products

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import data from './data.json';
 import Products from './components/Products';
 import Filter from './components/Filter';
 import Cart from './components/Cart';
+import store from "./store";
+import {Provider} from "react-redux";
 //Feature 1
 class App extends React.Component {
   constructor(){
@@ -69,6 +71,7 @@ class App extends React.Component {
   }
   render(){
         return (
+          <Provider store={store}>
           <div className="grid-container">
             <header>
               <div className="brand">
@@ -104,6 +107,7 @@ class App extends React.Component {
             </footer>
           
           </div>
+          </Provider>
         );
   }
 }
